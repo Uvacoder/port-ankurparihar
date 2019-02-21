b4eb4371c8bbcc538733e86a00e23ee66daafa5d
Ankur Parihar <26161041+ankurparihar@users.noreply.github.com>
2019-02-21 23:37:42 +0530

    Upload content

--- res-iitr/script.js
+++ res-iitr/script.js
@@ -267,6 +267,7 @@ var Semesters = {
 				Semester: "Third Year - 2nd Sem (Spring)",
 				Content: {
 					"Notes": "https://drive.google.com/drive/folders/126q9B-ldG29IRZ8Pg48GaVStMX_PAEoI?usp=sharing",
+					"Slides": "https://drive.google.com/open?id=10V-oWqc5ijYhIip_79NpAfoEHEwAWnGP",
 					"Assignment": "https://drive.google.com/drive/folders/1VlobDAV6XXRsdMSfPjkuD8NMaDmT9EqL?usp=sharing",
 					"Project": "/demo?origin=IES-301"
 				}
