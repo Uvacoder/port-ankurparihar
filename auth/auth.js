bac303b95e7e1bc1d4da4ad2cfd12637010a2d89
ankurparihar <pariharankur101@gmail.com>
2019-07-29 22:41:57 +0530

    Minified resources

--- auth/auth.js
+++ auth/auth.js
@@ -1,12 +1,1 @@
-function scanAuthBody() {
-	var params, key;
-	params = JSON.parse(document.body.innerText);
-	if (params['access_token']) {
-		// document.body.innerHTML = 'Access Token: ' + params['access_token'];
-		window.location.href = 'https://ankurparihar.github.io/auth/index.html?access_token=' + params['access_token'];
-	}
-	else if (params['error']) {
-		document.body.innerHTML = '<a id="auth" href="https://github.com/login/oauth/authorize?client_id=ac8e2ed8d62d346e1595&scope=repo">Authorise with Github</a>';
-	}
-}
-scanAuthBody();
+function scanAuthBody(){var t;(t=JSON.parse(document.body.innerText)).access_token?window.location.href="https://ankurparihar.github.io/auth/index.html?access_token="+t.access_token:t.error&&(document.body.innerHTML='<a id="auth" href="https://github.com/login/oauth/authorize?client_id=ac8e2ed8d62d346e1595&scope=repo">Authorise with Github</a>')}scanAuthBody();
